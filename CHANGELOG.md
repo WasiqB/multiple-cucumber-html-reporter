34295a42fa461db6f66a758b4d9b961dc595dd4d
Wim Selles <wim@wswebcreation.nl>
2017-05-21 08:49:50 +0200

    chore: initial release of gh-pages

--- CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-<a name="0.1.0"></a>
-
-### Features
-
-* **initial:** initial version of `Multiple Cucumer HTML Reporter`
-
-
-
--- test/test.js
+++
@@ -1,7 +0,0 @@
-const test = require('../lib/generate-report');
-
-test.generate({
-    saveCollectedJSON: true,
-    jsonDir: './test/unit/data/ns/',
-    reportPath: './.tmp/'
-});
