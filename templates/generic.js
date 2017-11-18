9b2364ed969bf5d61ea1b0882066f22e97a3949d
yon.cuadrado <yon.cuadrado@gmail.com>
2017-11-18 23:40:37 +0100

    initial commit

--- templates/generic.js
+++ templates/generic.js
@@ -2,15 +2,26 @@ $('.x_title').on('click', function () {
     var $BOX_PANEL = $(this).closest('.x_panel'),
         $ICON = $(this).find('.collapse-link i'),
         $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+        $BOX_CONTENT_1 = $BOX_PANEL.find('.x_content_new');
 
     // fix for some div with hardcoded fix class
     if ($BOX_PANEL.attr('style')) {
         $BOX_CONTENT.slideToggle(200, function () {
             $BOX_PANEL.removeAttr('style');
+            $BOX_PANEL.css('border', '2px solid');
+        });
+        
+        $BOX_CONTENT_1.slideToggle(200, function () {
+            $BOX_PANEL.removeAttr('style');
+            $BOX_PANEL.css('display', 'block');
+            $BOX_PANEL.css('border', '2px solid');
         });
     } else {
         $BOX_CONTENT.slideToggle(200);
+        // $BOX_CONTENT_1.slideToggle(200);
         $BOX_PANEL.css('height', 'auto');
+        $BOX_PANEL.css('display', 'block');
+        $BOX_PANEL.css('border', '2px solid');
     }
 
     $ICON.toggleClass('fa-chevron-up fa-chevron-down');
--- test/test.js
+++ test/test.js
@@ -5,5 +5,11 @@ const test = require('../lib/generate-report');
 test.generate({
     saveCollectedJSON: true,
     jsonDir: './test/unit/data/json/',
-    reportPath: './.tmp/'
+    reportPath: './.tmp/',
+    featuresFolder: './test/unit/data/features-scenarios-outline',
+    disableMetadataIfnotPresent: true,
+    showExecutionTime: true,
+    saveCollectedJSON:true,
+    expandStepsNotPassed: true,
+    navigateToFeatureIfThereIsOnlyOne:true
 });
