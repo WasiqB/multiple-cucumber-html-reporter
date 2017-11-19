632e1c3b14b3e801859f1f7afc637ff3169e1840
yon.cuadrado <yon.cuadrado@gmail.com>
2017-11-19 12:46:39 +0100

    unit tests fixes

--- test/test.js
+++ test/test.js
@@ -4,25 +4,18 @@ const test = require('../lib/generate-report');
 
 test.generate({
     saveCollectedJSON: true,
-    jsonDir: './test/unit/data/json/',
-    reportPath: './.tmp/',
-    featuresFolder: './test/unit/data/features-scenarios-outline',
-    disableMetadataIfnotPresent: true,
-    showExecutionTime: true,
-    saveCollectedJSON:true,
-    expandStepsNotPassed: true,
-    navigateToFeatureIfThereIsOnlyOne:true,
-    openReportInBrowser:true,
-    reportName: 'You can adjust this report name',
-    customData: {
-        title: 'Run info',
-        data: [
-            {label: 'Project', value: 'Custom project'},
-            {label: 'Release', value: '1.2.3'},
-            {label: 'Cycle', value: 'B11221.34321'},
-            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
-            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
-        ]
+    jsonDir: './unit/data/collect-json',
+    reportPath: './reportPath',
+    metadata:{
+        browser: {
+            name: 'chrome',
+            version: '1'
+        },
+        device: 'Local test machine',
+        platform: {
+            name: 'Ubuntu',
+            version: '16.04'
+        }
     }
 
 });
