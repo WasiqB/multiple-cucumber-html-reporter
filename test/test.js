a2d4659ccbd1acc0a0353e624a9a3a862320fa51
yon.cuadrado <yon.cuadrado@gmail.com>
2017-11-19 12:50:16 +0100

    unit tests fixes

--- test/test.js
+++ test/test.js
@@ -4,18 +4,17 @@ const test = require('../lib/generate-report');
 
 test.generate({
     saveCollectedJSON: true,
-    jsonDir: './unit/data/collect-json',
-    reportPath: './reportPath',
-    metadata:{
-        browser: {
-            name: 'chrome',
-            version: '1'
-        },
-        device: 'Local test machine',
-        platform: {
-            name: 'Ubuntu',
-            version: '16.04'
-        }
+    jsonDir: './test/unit/data/json/',
+    reportPath: './.tmp/',
+    reportName: 'You can adjust this report name',
+    customData: {
+        title: 'Run info',
+        data: [
+            {label: 'Project', value: 'Custom project'},
+            {label: 'Release', value: '1.2.3'},
+            {label: 'Cycle', value: 'B11221.34321'},
+            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
+            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+        ]
     }
-
 });
