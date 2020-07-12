ea2abb22029ce026633c21a97e02c3318d982abc
hdorgeval <henri.dorgeval@outlook.fr>
2020-07-12 21:59:56 +0200

    test(custom-template): add tests for customTemplate option

--- test/test.js
+++ test/test.js
@@ -7,44 +7,70 @@ const test = require('../lib/generate-report');
  * Generate a report for browsers
  */
 test.generate({
-    saveCollectedJSON: true,
-    jsonDir: './test/unit/data/json/',
-    reportPath: './.tmp/browsers/',
-    reportName: 'You can adjust this report name',
-    customMetadata: false,
-    displayDuration: true,
-    durationInMS: true,
-    customData: {
-        title: 'Run info',
-        data: [
-            {label: 'Project', value: 'Custom project'},
-            {label: 'Release', value: '1.2.3'},
-            {label: 'Cycle', value: 'B11221.34321'},
-            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
-            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
-        ]
-    }
+	saveCollectedJSON: true,
+	jsonDir: './test/unit/data/json/',
+	reportPath: './.tmp/browsers/',
+	reportName: 'You can adjust this report name',
+	customMetadata: false,
+	displayDuration: true,
+	durationInMS: true,
+	customData: {
+		title: 'Run info',
+		data: [
+			{label: 'Project', value: 'Custom project'},
+			{label: 'Release', value: '1.2.3'},
+			{label: 'Cycle', value: 'B11221.34321'},
+			{label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
+			{label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+		]
+	}
+});
+
+/**
+ * Generate a report for browsers with a custom template
+ */
+test.generate({
+	saveCollectedJSON: true,
+	jsonDir: './test/unit/data/json/',
+	reportPath: './.tmp/browsers/custom-templates/',
+	reportName: 'You can adjust this report name',
+	customMetadata: false,
+	displayDuration: true,
+	durationInMS: true,
+	customData: {
+		title: 'Run info',
+		data: [
+			{label: 'Project', value: 'Custom project'},
+			{label: 'Release', value: '1.2.3'},
+			{label: 'Cycle', value: 'B11221.34321'},
+			{label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
+			{label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+		]
+	},
+	customTemplate: {
+		featuresOverview: path.join(__dirname, 'custom-templates', 'features-overview.tmpl')
+	}
 });
 
 /**
  * Generate a report with array of embedded data
  */
 test.generate({
-    saveCollectedJSON: true,
-    jsonDir: './test/unit/data/embedded-array-json/',
-    reportPath: './.tmp/embedded-array/',
-    customStyle: path.join(__dirname, './custom.css'),
-    overrideStyle: path.join(__dirname, './my.css'),
-    customMetadata: false,
-    pageTitle: 'A custom page title',
-    pageFooter: '<div><p>Some custom footer data can be placed here</p></div>',
-    customData: {
-        title: 'Run info',
-        data: [
-            {label: 'Project', value: 'Custom embedded project'},
-            {label: 'Release', value: '4.5.6'},
-        ]
-    }
+	saveCollectedJSON: true,
+	jsonDir: './test/unit/data/embedded-array-json/',
+	reportPath: './.tmp/embedded-array/',
+	customStyle: path.join(__dirname, './custom.css'),
+	overrideStyle: path.join(__dirname, './my.css'),
+	customMetadata: false,
+	pageTitle: 'A custom page title',
+	pageFooter: '<div><p>Some custom footer data can be placed here</p></div>',
+	customData: {
+		title: 'Run info',
+		data: [
+			{label: 'Project', value: 'Custom embedded project'},
+			{label: 'Release', value: '4.5.6'}
+		]
+	}
 });
 
 /**
@@ -52,9 +78,9 @@ test.generate({
  * NOTE: must be last, if you use customMetadata you cannot reuse generator
  */
 test.generate({
-    saveCollectedJSON: true,
-    jsonDir: './test/unit/data/custom-metadata-json/',
-    reportPath: './.tmp/custom-metadata/',
-    customMetadata: true,
-    displayDuration: true
+	saveCollectedJSON: true,
+	jsonDir: './test/unit/data/custom-metadata-json/',
+	reportPath: './.tmp/custom-metadata/',
+	customMetadata: true,
+	displayDuration: true
 });
--- test/unit/generate-json.spec.js
+++ test/unit/generate-json.spec.js
@@ -6,96 +6,178 @@ const multiCucumberHTMLReporter = require('../../lib/generate-report');
 const REPORT_PATH = './.tmp/';
 
 describe('generate-report.js', () => {
-    describe('Happy flows', () => {
-        it('should create a report from the merged found json files without provided custom data', () => {
-            fs.removeSync(REPORT_PATH);
-            multiCucumberHTMLReporter.generate({
-                jsonDir: './test/unit/data/json',
-                reportPath: REPORT_PATH,
-                saveCollectedJSON: true,
-                displayDuration: true
-            });
+	describe('Happy flows', () => {
+		it('should create a report from the merged found json files without provided custom data', () => {
+			fs.removeSync(REPORT_PATH);
+			multiCucumberHTMLReporter.generate({
+				jsonDir: './test/unit/data/json',
+				reportPath: REPORT_PATH,
+				saveCollectedJSON: true,
+				displayDuration: true
+			});
 
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile())
-                .toEqual(true, 'Index file exists');
-            expect(function() { fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'features/happy-flow-v2.html')}`); })
-                .toThrow();
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'merged-output.json')}`).isFile())
-                .toEqual(true, 'merged-output.json file exists');
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'enriched-output.json')}`).isFile())
-                .toEqual(true, 'temp-output.json file exists');
-        });
-        it('should create a report from the merged found json files with custom data with static file paths', () => {
-            fs.removeSync(REPORT_PATH);
-            multiCucumberHTMLReporter.generate({
-                jsonDir: './test/unit/data/json',
-                reportPath: REPORT_PATH,
-                staticFilePath: true,
-                saveCollectedJSON: true,
-                reportName: 'You can adjust this report name',
-                customData: {
-                    title: 'Run info',
-                    data: [
-                        {label: 'Project', value: 'Custom project'},
-                        {label: 'Release', value: '1.2.3'},
-                        {label: 'Cycle', value: 'B11221.34321'},
-                        {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
-                        {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
-                    ]
-                },
-                displayDuration: true,
-                durationInMS: true
-            });
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile()).toEqual(
+				true,
+				'Index file exists'
+			);
+			expect(function () {
+				fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'features/happy-flow-v2.html')}`);
+			}).toThrow();
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'merged-output.json')}`).isFile()).toEqual(
+				true,
+				'merged-output.json file exists'
+			);
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'enriched-output.json')}`).isFile()).toEqual(
+				true,
+				'temp-output.json file exists'
+			);
+		});
 
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile())
-                .toEqual(true, 'Index file exists');
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'features/happy-flow-v2.html')}`).isFile())
-                .toEqual(true, 'uuid free feature exists');
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'merged-output.json')}`).isFile())
-                .toEqual(true, 'merged-output.json file exists');
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'enriched-output.json')}`).isFile())
-                .toEqual(true, 'temp-output.json file exists');
-        });
-        it('should create a report from the merged found json files with custom metadata', () => {
-            fs.removeSync(REPORT_PATH);
-            multiCucumberHTMLReporter.generate({
-                jsonDir: './test/unit/data/custom-metadata-json/',
-                reportPath: REPORT_PATH,
-                customMetadata: true
-            });
+		it('should create a report from the merged found json files with custom template', () => {
+			fs.removeSync(REPORT_PATH);
+			multiCucumberHTMLReporter.generate({
+				jsonDir: './test/unit/data/json',
+				reportPath: REPORT_PATH,
+				displayDuration: true,
+				customTemplate: {
+					featuresOverview: path.join(__dirname, '..', 'custom-templates', 'features-overview.tmpl')
+				}
+			});
 
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile())
-                .toEqual(true, 'Index file exists');
-        });
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile()).toEqual(
+				true,
+				'Index file exists'
+			);
+			expect(fs.readFileSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`)).toContain('Custom Column');
+			expect(fs.readFileSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`)).toContain('Custom Content');
+		});
 
-        it('should create a report from the merged found json files and with array of embedded items', () => {
-            fs.removeSync(REPORT_PATH);
-            multiCucumberHTMLReporter.generate({
-                jsonDir: './test/unit/data/embedded-array-json/',
-                reportName: 'Embedded array of various mimeType',
-                reportPath: REPORT_PATH,
-                customStyle: path.join(__dirname, '../my.css'),
-                customMetadata: false
-            });
+		it('should create a report from the merged found json files with custom data with static file paths', () => {
+			fs.removeSync(REPORT_PATH);
+			multiCucumberHTMLReporter.generate({
+				jsonDir: './test/unit/data/json',
+				reportPath: REPORT_PATH,
+				staticFilePath: true,
+				saveCollectedJSON: true,
+				reportName: 'You can adjust this report name',
+				customData: {
+					title: 'Run info',
+					data: [
+						{label: 'Project', value: 'Custom project'},
+						{label: 'Release', value: '1.2.3'},
+						{label: 'Cycle', value: 'B11221.34321'},
+						{
+							label: 'Execution Start Time',
+							value: 'Nov 19th 2017, 02:31 PM EST'
+						},
+						{
+							label: 'Execution End Time',
+							value: 'Nov 19th 2017, 02:56 PM EST'
+						}
+					]
+				},
+				displayDuration: true,
+				durationInMS: true
+			});
 
-            expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile())
-                .toEqual(true, 'Index file exists');
-        });
-    });
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile()).toEqual(
+				true,
+				'Index file exists'
+			);
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'features/happy-flow-v2.html')}`).isFile()).toEqual(
+				true,
+				'uuid free feature exists'
+			);
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'merged-output.json')}`).isFile()).toEqual(
+				true,
+				'merged-output.json file exists'
+			);
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'enriched-output.json')}`).isFile()).toEqual(
+				true,
+				'temp-output.json file exists'
+			);
+		});
+		it('should create a report from the merged found json files with custom metadata', () => {
+			fs.removeSync(REPORT_PATH);
+			multiCucumberHTMLReporter.generate({
+				jsonDir: './test/unit/data/custom-metadata-json/',
+				reportPath: REPORT_PATH,
+				customMetadata: true
+			});
 
-    describe('failures', () => {
-        it('should throw an error when no options are provided', () => {
-            expect(() => multiCucumberHTMLReporter.generate()).toThrowError('Options need to be provided.');
-        });
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile()).toEqual(
+				true,
+				'Index file exists'
+			);
+		});
 
-        it('should throw an error when the json folder does not exist', () => {
-            expect(() => multiCucumberHTMLReporter.generate({})).toThrowError(`A path which holds the JSON files should be provided.`);
-        });
+		it('should create a report from the merged found json files with custom metadata and custom template', () => {
+			fs.removeSync(REPORT_PATH);
+			multiCucumberHTMLReporter.generate({
+				jsonDir: './test/unit/data/custom-metadata-json/',
+				reportPath: REPORT_PATH,
+				customMetadata: true,
+				customTemplate: {
+					featuresOverview: path.join(__dirname, '..', 'custom-templates', 'features-overview-custom-metadata.tmpl')
+				}
+			});
 
-        it('should throw an error when the report folder is not provided', () => {
-            expect(() => multiCucumberHTMLReporter.generate({
-                jsonDir: './test/unit/data/json'
-            })).toThrowError(`An output path for the reports should be defined, no path was provided.`);
-        });
-    });
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile()).toEqual(
+				true,
+				'Index file exists'
+			);
+			expect(fs.readFileSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`)).toContain('Custom Column');
+			expect(fs.readFileSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`)).toContain('Custom Content');
+		});
+
+		it('should create a report from the merged found json files and with array of embedded items', () => {
+			fs.removeSync(REPORT_PATH);
+			multiCucumberHTMLReporter.generate({
+				jsonDir: './test/unit/data/embedded-array-json/',
+				reportName: 'Embedded array of various mimeType',
+				reportPath: REPORT_PATH,
+				customStyle: path.join(__dirname, '../my.css'),
+				customMetadata: false
+			});
+
+			expect(fs.statSync(`${path.join(process.cwd(), REPORT_PATH, 'index.html')}`).isFile()).toEqual(
+				true,
+				'Index file exists'
+			);
+		});
+	});
+
+	describe('failures', () => {
+		it('should throw an error when no options are provided', () => {
+			expect(() => multiCucumberHTMLReporter.generate()).toThrowError('Options need to be provided.');
+		});
+
+		it('should throw an error when the json folder does not exist', () => {
+			expect(() => multiCucumberHTMLReporter.generate({})).toThrowError(
+				`A path which holds the JSON files should be provided.`
+			);
+		});
+
+		it('should throw an error when the report folder is not provided', () => {
+			expect(() =>
+				multiCucumberHTMLReporter.generate({
+					jsonDir: './test/unit/data/json'
+				})
+			).toThrowError(`An output path for the reports should be defined, no path was provided.`);
+		});
+
+		it('should throw an error when custom template file does not exist', () => {
+			fs.removeSync(REPORT_PATH);
+			expect(() =>
+				multiCucumberHTMLReporter.generate({
+					jsonDir: './test/unit/data/json',
+					reportPath: REPORT_PATH,
+					displayDuration: true,
+					customTemplate: {
+						featuresOverview: 'foobar'
+					}
+				})
+			).toThrowError("Cannot read the custom template file 'foobar'");
+		});
+	});
 });
