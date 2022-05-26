f265238cbe5638064263f5eba8834035f02f2713
Bhaslamb <baskercarrer@gmail.com>
2022-05-26 12:41:14 +0200

    Dark theme added by toggle option * fixed - as per suggestion.

--- templates/assets/js/darkmode.js
+++ templates/assets/js/darkmode.js
@@ -1,11 +1,6 @@
 var darkMode = 'darkmode'
-var elementList = ['div.x_panel', 'div.x_title','div.container-fluid', 'div.created-by']
-var featureElementList = ['div.x_panel','div.x_title', 'div.main_conainer', 'div.container-fluid', 'div.created-by', 'small'];
 
 function applyDarkMode() {
-    elementList.forEach(value => {
-        document.querySelectorAll(value).forEach(value => value.classList.toggle(darkMode));
-    });
     document.getElementById('features-table').classList.toggle('table-striped');
     document.getElementById('darkmodeicon')?.classList.toggle('fa-toggle-on');
     applyFontStyle();
@@ -23,12 +18,6 @@ function applyFontStyle() {
     document.body.classList.toggle(darkMode);
 }
 
-
-function applyDarkModeInFeature() {
-    featureElementList.forEach(value => {
-        document.querySelectorAll(value).forEach(value => value.classList.toggle(darkMode));
-    });
-}
 
 function isDarkModeOn() {
     var toggle = document.getElementById('darkCheck');
