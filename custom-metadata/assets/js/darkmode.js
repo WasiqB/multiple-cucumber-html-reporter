0ec6f2f88666f3d8a532a3f46b8f668f2a038c72
Wim Selles <wim@wswebcreation.nl>
2022-05-27 08:57:13 +0200

    add dark mode

---
+++ custom-metadata/assets/js/darkmode.js
@@ -0,0 +1,32 @@
+var darkMode = 'darkmode'
+
+function applyDarkMode() {
+    document.getElementById('features-table').classList.toggle('table-striped');
+    document.getElementById('darkmodeicon')?.classList.toggle('fa-toggle-on');
+    applyFontStyle();
+}
+
+function saveState() {
+    if(isDarkModeOn()) {
+        window.localStorage['darkmode'] = 'on';
+    } else {
+        window.localStorage['darkmode'] = 'off';
+    }
+}
+
+function applyFontStyle() {
+    document.body.classList.toggle(darkMode);
+}
+
+
+function isDarkModeOn() {
+    var toggle = document.getElementById('darkCheck');
+    return toggle.checked;
+}
+
+window.onload = function() {
+    if(window.localStorage['darkmode'] === 'on') {
+        applyDarkMode();
+        document.getElementById('darkCheck').checked = true;
+    }
+}
---
+++ embedded-array/assets/js/Chart.style.js
@@ -0,0 +1,16 @@
+function getChartColors() {
+    const colorsMap = [
+        {colorVar: "--passed-color", defaultColor: "#26B99A"},
+        {colorVar: "--failed-color", defaultColor: "#E74C3C"},
+        {colorVar: "--pending-color", defaultColor: "#FFD119"},
+        {colorVar: "--skipped-color", defaultColor: "#3498DB"},
+        {colorVar: "--ambiguous-color", defaultColor: "#b73122"},
+        {colorVar: "--not-defined-color", defaultColor: "#F39C12"},
+    ];
+    const colors = []
+    const style = window.getComputedStyle(document.body);
+    for (let i = 0; i < colorsMap.length; i++) {
+         colors.push(style.getPropertyValue(colorsMap[i].colorVar) || colorsMap[i].defaultColor)
+    }
+    return colors
+}
