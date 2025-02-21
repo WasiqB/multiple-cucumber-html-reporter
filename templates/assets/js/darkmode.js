1bbcd5e805d68804f33bbf15d8eca170748419c7
Wasiq Bhamla <wasbhamla2005@gmail.com>
2025-02-21 22:18:23 +0300

    fix: :bug: fixed dark mode issue with Data Table (#435)
    * fix: :bug: fixed dark mode issue with Data Table
    
    * fix: :bug: fixed issue with dark mode when refreshing the page

--- templates/assets/js/darkmode.js
+++ templates/assets/js/darkmode.js
@@ -1,31 +1,36 @@
-var darkMode = 'darkmode'
+var darkMode = "darkmode";
 
 function applyDarkMode() {
-    document.getElementById('features-table').classList.toggle('table-striped');
-    applyFontStyle();
+  applyFontStyle();
+  document
+    .querySelector("html")
+    .setAttribute("data-bs-theme", isDarkModeOn() ? "dark" : "light");
+  document.getElementById("features-table").classList.toggle("table-striped");
 }
 
 function saveState() {
-    if(isDarkModeOn()) {
-        window.localStorage['darkmode'] = 'on';
-    } else {
-        window.localStorage['darkmode'] = 'off';
-    }
+  if (isDarkModeOn()) {
+    window.localStorage["darkmode"] = "on";
+  } else {
+    window.localStorage["darkmode"] = "off";
+  }
 }
 
 function applyFontStyle() {
-    document.body.classList.toggle(darkMode);
+  document.body.classList.toggle(darkMode);
 }
-
 
 function isDarkModeOn() {
-    var toggle = document.getElementById('darkCheck');
-    return toggle.checked;
+  var toggle = document.getElementById("darkCheck");
+  return toggle.checked;
 }
 
-window.onload = function() {
-    if(window.localStorage['darkmode'] === 'on') {
-        applyDarkMode();
-        document.getElementById('darkCheck').checked = true;
-    }
-}
+window.onload = function () {
+  if (window.localStorage["darkmode"] === "on") {
+    applyDarkMode();
+    document.getElementById("darkCheck").checked = true;
+    document
+      .querySelector("html")
+      .setAttribute("data-bs-theme", isDarkModeOn() ? "dark" : "light");
+  }
+};
