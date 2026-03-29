b8780b0b86d8ceb26a88b72077db25f9a05e06b3
WasiqB <wasbhamla2005@gmail.com>
2026-03-29 21:06:39 +0530

    feat: :sparkles: adding docs for the project

---
+++ docs/proxy.ts
@@ -0,0 +1,23 @@
+import { isMarkdownPreferred, rewritePath } from 'fumadocs-core/negotiation';
+import { type NextRequest, NextResponse } from 'next/server';
+import { docsContentRoute, docsRoute } from '@/lib/shared';
+
+const { rewrite: rewriteDocs } = rewritePath(`${docsRoute}{/*path}`, `${docsContentRoute}{/*path}/content.md`);
+const { rewrite: rewriteSuffix } = rewritePath(`${docsRoute}{/*path}.mdx`, `${docsContentRoute}{/*path}/content.md`);
+
+export default function proxy(request: NextRequest) {
+  const result = rewriteSuffix(request.nextUrl.pathname);
+  if (result) {
+    return NextResponse.rewrite(new URL(result, request.nextUrl));
+  }
+
+  if (isMarkdownPreferred(request)) {
+    const result = rewriteDocs(request.nextUrl.pathname);
+
+    if (result) {
+      return NextResponse.rewrite(new URL(result, request.nextUrl));
+    }
+  }
+
+  return NextResponse.next();
+}
---
+++ docs/src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = 'My App';
+export const docsRoute = '/docs';
+export const docsImageRoute = '/og/docs';
+export const docsContentRoute = '/llms.mdx/docs';
+
+export const gitConfig = {
+  user: 'WasiqB',
+  repo: 'multiple-cucumber-html-reporter',
+  branch: 'main',
+};
