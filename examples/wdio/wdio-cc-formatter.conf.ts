fef19f7d386a5c0c554af21189df3b5de49b9bfe
WasiqB <wasbhamla2005@gmail.com>
2026-07-02 16:37:24 +0530

    feat: :sparkles: handled unique report metadata for wdio when executing on multiple browsers on BS

--- examples/wdio/wdio-cc-formatter.conf.ts
+++ examples/wdio/wdio-cc-formatter.conf.ts
@@ -4,6 +4,10 @@ export const config: WebdriverIO.Config = {
   ...sharedConfig,
   cucumberOpts: {
     ...sharedConfig.cucumberOpts,
-    format: ['progress', 'json:reports/json/cucumber-report.json', 'message:reports/message/cucumber-message.ndjson'],
+    format: [
+      'progress',
+      `json:reports/json/cucumber-report-${process.env.WDIO_WORKER_ID}.json`,
+      `message:reports/message/cucumber-message-${process.env.WDIO_WORKER_ID}.ndjson`,
+    ],
   },
 };
